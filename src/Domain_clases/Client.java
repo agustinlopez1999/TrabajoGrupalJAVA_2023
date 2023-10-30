25f92b9ed36bb05915607f9d1cfc2635e0118cb0
crisceri123 <criscerillano@gmail.com>
2023-10-30 17:21:53 -0300

    comit_cris

---
+++ src/Domain_clases/Client.java
@@ -0,0 +1,61 @@
+package Domain_clases;
+
+public class Client {
+    private int number;
+    private String Name;
+    private String LastName;
+    private String Bussines;
+
+    public Client(int number, String name, String lastName) {
+        this.number = number;
+        Name = name;
+        LastName = lastName;
+    }
+
+    public Client(int number, String bussines) {
+        this.number = number;
+        Bussines = bussines;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public String getBussines() {
+        return Bussines;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public void setBussines(String bussines) {
+        Bussines = bussines;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "number=" + number +
+                ", Name='" + Name + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", Bussines='" + Bussines + '\'' +
+                '}';
+    }
+}
--- src/Domain_clases/Clientes.java
+++
@@ -1,4 +0,0 @@
-package Domain_clases;
-
-public class Clientes {
-}
