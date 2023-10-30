07e08155c26b510a6d63b0673dcf4a2eec2e5b74
Agustin <lopezagustin1999@hotmail.com>
2023-10-30 17:22:12 -0300

    push agus

---
+++ src/Domain_clases/Stand.java
@@ -0,0 +1,27 @@
+package Domain_clases;
+
+public class Stand {
+    private String code;
+    private float surface;
+    private float m2price;
+
+    public Stand() {
+    }
+    public Stand(String code, float surface, float m2price) {
+        this.code = code;
+        this.surface = surface;
+        this.m2price = m2price;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setSurface(float surface) {
+        this.surface = surface;
+    }
+
+    public void setM2price(float m2price) {
+        this.m2price = m2price;
+    }
+}
