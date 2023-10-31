17b9a3f8256fc40c35affa134c5b5723af67d5d7
crisceri123 <criscerillano@gmail.com>
2023-10-31 16:19:31 -0300

    Borro lo comentado del main

--- src/Main.java
+++ src/Main.java
@@ -11,34 +11,6 @@ public class Main {
             System.out.println("No se han cargado stands.");
         } else {
             fair.show();
-            /*
-            ArrayList<Stand> stands = fair.getStands();
-            for (Stand stand : stands) {
-                System.out.println("Stand Details:");
-                System.out.println("Code: " + stand.getCode());
-                System.out.println("Surface: " + stand.getSurface());
-                System.out.println("M2 Price: " + stand.getM2price());
-
-                if (stand instanceof Inside insideStand) {
-                    System.out.println("Luminaries: " + insideStand.getLuminaries());
-                }
-
-                System.out.println("Accessories:");
-                ArrayList<Accessory> accessories = stand.getAccesories();
-                for (Accessory accessory : accessories) {
-                    System.out.println("ID: " + accessory.getId());
-                    System.out.println("Description: " + accessory.getDescription());
-                    System.out.println("Price: " + accessory.getPrice());
-                }
-
-                Client client = stand.getStandClient();
-                System.out.println("Client:");
-                System.out.println("Client Number: " + client.getNumber());
-                System.out.println("Client Name: " + client.getName());
-
-                System.out.println();
-            }
-            */
         }
     }
 }
