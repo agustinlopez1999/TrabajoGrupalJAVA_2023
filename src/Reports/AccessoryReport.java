673ec1bccae1e3bd02366fd96a7ce87241a36d5c
UlisesFritzSerain <ulifritz8@gmail.com>
2023-11-02 15:40:18 -0300

    reporte txt de accesorios

--- src/Reports/AccessoryReport.java
+++ src/Reports/AccessoryReport.java
@@ -1,20 +1,29 @@
 package Reports;
 import Domain_classes.*;
 import Comparator.AccessoriesComparator;
-import java.util.HashMap;
-import java.util.Map;
+
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.TreeMap;
 
 public class AccessoryReport {
     AccessoriesComparator accessoriesComparator = new AccessoriesComparator();
     TreeMap<Accessory, Integer> hm = new TreeMap<Accessory, Integer>(accessoriesComparator);
 
-    public void showReportAccessories(Fair fair){
+    public void showReportAccessories(Fair fair, String fileName){
         TreeMap<Accessory,Integer>  tm =  fair.loadAccessories();
         int value;
-        for (Accessory aux : tm.keySet()){
-            value = tm.get(aux);
-            System.out.println(aux + ": " + value);
+        try (FileWriter fileWriter = new FileWriter(fileName, false)) {
+            for (Accessory aux : tm.keySet()) {
+                value = tm.get(aux);
+                String reportLine = aux + ": " + value;
+                System.out.println(reportLine);
+                fileWriter.write(reportLine + "\n");
+            }
+
+            System.out.println("Reporte de accesorios guardado en '" + fileName + "'");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
